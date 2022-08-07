ebf9690f55b3b79f807450f9eedd945a1618ac78
‘MrYz0’ <2569901191@qq.com>
2022-08-07 10:12:50 +0800

    Initial commit

---
+++ ruoyi-ui/src/views/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="app-container home">
+  <h1>首页</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  data() {
+    return {
+      // 版本号
+      version: "3.8.3",
+    };
+  },
+  methods: {
+    goTarget(href) {
+      window.open(href, "_blank");
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
+
